442c407821452c60ee986e343dfa414e67aaccc0
@toastytentacles <marq46.steam@googlemail.com>
2015-07-03 00:28:14 +0000

    changes?

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // use std::iter;
 use std::io::*;
 use std::net::TcpStream;
+use std::path::Path;
+use std::fs::File;
 
 fn main() {
 	let mut stream = TcpStream::connect("irc.freenode.net:6667").unwrap();
@@ -13,17 +15,54 @@ fn main() {
 	let _ = stream.write_all(format!("USER {n} {n} {n} :meow\r\n", n = name).as_bytes());
 	let _ = stream.write_all(format!("JOIN {c}\r\n", c = channel).as_bytes());
 
+	let log_path = Path::new("log.txt");
+
+	let mut log_file = match File::create(&log_path) {
+		Err(why) => (panic!("error {}", why)),
+		Ok(log_file) => log_file,
+	};
+
 	let mut read_head = BufReader::new(stream);
+
+	let _ = stream.write_all("thsi is cats".as_bytes());
+
 	while true {
 		let mut take = String::new();
+		// let _ = stream.read_to_string(&mut take);
+		// let mut read_head = BufReader::new(take);
+
+		// let take_line = String::new();
+		// let _ = read_head.read_line(&mut take_line).unwrap();
+
 		read_head.read_line(&mut take).unwrap();
-		// println!("{}", take);
+		log_file.write_all(take.as_bytes());
 
-		if take.contains("go to sleep") { break; }
+		// if take.contains("go to sleep") { break; }
 
-		let read_stack: Vec<&str> = take.splitn(3, ":").collect();
-		if read_stack.len() == 3 {
-			println!("{}", read_stack[2]);
-		}
+		// if check_pm(take, "test") {
+		// 	send_msg(&stream, "yah that worked", channel);
+		// }
 	}
 }
+
+// fn check_pm(t: String, msg: &str) -> bool {
+// 	let mut tick_list = false;
+
+// 	let read_stack: Vec<&str> = t.splitn(4, " ").collect();
+// 	if read_stack.len() == 4 {
+// 		if read_stack[1].contains(" PRIVMSG ") { 
+// 			if read_stack[3].contains(msg) {
+// 				tick_list = true;
+// 			}
+// 		}
+// 	}
+// 	else {
+// 		tick_list = false;
+// 	}
+
+// 	return tick_list;
+// }
+
+// fn send_msg(s: &TcpStream, msg: &str, channel: &str) {
+// 	s.write_all(format!("PRIVMSG {c} :{m}\r\n", c = channel, m = msg).as_bytes());
+// }
