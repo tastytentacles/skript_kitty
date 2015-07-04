b4dbbc3946d781954f680446d69cfb9f200c3ba2
Q <marq46.steam@googlemail.com>
2015-07-05 00:52:43 +0100

    working send MSG function, removerd log cos problems

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 use std::fs::File;
 
 fn main() {
-	let mut stream = TcpStream::connect("irc.freenode.net:6667").unwrap();
+	let mut stream :TcpStream = TcpStream::connect("irc.freenode.net:6667").unwrap();
 
 	let name = "skript_kitty";
 	let channel = "#testingsnoopyq";
@@ -15,42 +15,51 @@ fn main() {
 	let _ = stream.write_all(format!("USER {n} {n} {n} :meow\r\n", n = name).as_bytes());
 	let _ = stream.write_all(format!("JOIN {c}\r\n", c = channel).as_bytes());
 
-	let log_path = Path::new("log.txt");
+	// let log_path = Path::new("log.txt");
+	//
+	// let mut log_file = match File::create(&log_path) {
+	// 	Err(why) => (panic!("error {}", why)),
+	// 	Ok(log_file) => log_file,
+	// };
 
-	let mut log_file = match File::create(&log_path) {
-		Err(why) => (panic!("error {}", why)),
-		Ok(log_file) => log_file,
-	};
-
-	let mut read_head = BufReader::new(stream);
-
-	let _ = stream.write_all("thsi is cats".as_bytes());
+	let read_stream = stream.try_clone().unwrap();
+	let mut read_head = BufReader::new(read_stream);
+	let mut write_head = BufWriter::new(stream);
 
 	while true {
 		let mut take = String::new();
-		// let _ = stream.read_to_string(&mut take);
-		// let mut read_head = BufReader::new(take);
-
-		// let take_line = String::new();
-		// let _ = read_head.read_line(&mut take_line).unwrap();
-
 		read_head.read_line(&mut take).unwrap();
-		log_file.write_all(take.as_bytes());
 
-		// if take.contains("go to sleep") { break; }
+		println!("{}", take);
+		// let _ = log_file.write_all(take.as_bytes());
+
+		if take.contains("go to sleep") {
+			send_msg(&mut write_head, "ok going to sleep", channel);
+			break;
+		}
 
 		// if check_pm(take, "test") {
-		// 	send_msg(&stream, "yah that worked", channel);
+		// 	// send_msg(stream, "yah that worked", channel);
+		// 	println!("yah that worked");
 		// }
 	}
 }
 
+// fn parse_type() -> u16 {
+//
+// }
+
+fn send_msg(s: &mut BufWriter<TcpStream>, msg: &str, channel: &str) {
+	s.write_all(format!("PRIVMSG {c} :{m}\r\n", c = channel, m = msg).as_bytes()).unwrap();
+	s.flush().unwrap();
+}
+
 // fn check_pm(t: String, msg: &str) -> bool {
 // 	let mut tick_list = false;
-
+//
 // 	let read_stack: Vec<&str> = t.splitn(4, " ").collect();
 // 	if read_stack.len() == 4 {
-// 		if read_stack[1].contains(" PRIVMSG ") { 
+// 		if read_stack[1].contains(" PRIVMSG ") {
 // 			if read_stack[3].contains(msg) {
 // 				tick_list = true;
 // 			}
@@ -59,10 +68,6 @@ fn main() {
 // 	else {
 // 		tick_list = false;
 // 	}
-
+//
 // 	return tick_list;
-// }
-
-// fn send_msg(s: &TcpStream, msg: &str, channel: &str) {
-// 	s.write_all(format!("PRIVMSG {c} :{m}\r\n", c = channel, m = msg).as_bytes());
 // }
