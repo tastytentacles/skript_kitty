fde6f57d797c4b7973a3be025f734b6e47d22086
Q <marq46.steam@googlemail.com>
2015-07-07 03:38:58 +0100

    over hall 0001

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,45 @@
 use std::io::*;
 use std::net::TcpStream;
 
+
+struct IRC {
+	stream :TcpStream,
+	read :BufReader<TcpStream>,
+	write :BufWriter<TcpStream>
+}
+
+static NAME :&'static str = "skript_kitty";
+static CHANNEL :&'static str = "#testingsnoopyq";
+
 fn main() {
-	let mut stream :TcpStream = TcpStream::connect("irc.freenode.net:6667").unwrap();
-
-	let name = "skript_kitty";
-	let channel = "#testingsnoopyq";
-	// let channel = "#pbsideachannel";
-
-	let _ = stream.write_all(format!("NICK {n}\r\n", n = name).as_bytes());
-	let _ = stream.write_all(format!("USER {n} {n} {n} :meow\r\n", n = name).as_bytes());
-	let _ = stream.write_all(format!("JOIN {c}\r\n", c = channel).as_bytes());
-
-	let read_stream = stream.try_clone().unwrap();
-	let mut read_head = BufReader::new(read_stream);
-	let mut write_head = BufWriter::new(stream);
+	let mut client = irc_new("irc.freenode.net:6667");
+	irc_join(&mut client);
 
 	loop {
 		let mut take = String::new();
-		read_head.read_line(&mut take).unwrap();
+		client.read.read_line(&mut take).unwrap();
 
 		println!("{}", take);
 
 		if take.starts_with("PING") {
-			let _ = write_head.write_all("PONG: nope".as_bytes());
-			let _ = write_head.flush();
+			let _ = client.write.write_all("PONG: nope".as_bytes());
+			let _ = client.write.flush();
 		}
 
+		// ---------------------------------- # kill command #
 		if msg_contains(&take, "go to sleep") {
-			msg_send(&mut write_head, "ok I am off to sleep", channel);
+			msg_send(&mut client.write, "ok I am off to sleep");
 			break; }
 
+		// ---------------------------------- # simple hello responce #
 		if msg_contains(&take, "hello")
-			{ msg_send(&mut write_head, "hello", channel); }
+			{ msg_send(&mut client.write, "hello"); }
 	}
 }
 
-fn msg_send(s: &mut BufWriter<TcpStream>, msg: &str, channel: &str) {
+fn msg_send(s: &mut BufWriter<TcpStream>, msg: &str) {
 	s.write_all(format!("PRIVMSG {c} :{m}\r\n",
-		c = channel, m = msg).as_bytes()).unwrap();
+		c = CHANNEL, m = msg).as_bytes()).unwrap();
 	s.flush().unwrap();
 }
 
@@ -51,4 +52,25 @@ fn msg_contains(s: &String, t: &str) -> bool {
 		return true;
 	}
 	else { return false; }
+}
+
+fn irc_new(url :&str) -> IRC {
+	let tcp = TcpStream::connect(url).unwrap();
+	let br = BufReader::new(tcp.try_clone().unwrap());
+	let bw = BufWriter::new(tcp.try_clone().unwrap());
+
+	let irc_out = IRC {
+		stream: tcp,
+		read: br,
+		write: bw
+	};
+
+	irc_out
+}
+
+fn irc_join(irc :&mut IRC) {
+	irc.write.write_all(format!("NICK {n}\r\n", n = NAME).as_bytes()).unwrap();
+	irc.write.write_all(format!("USER {n} {n} {n} :meow\r\n", n = NAME).as_bytes()).unwrap();
+	irc.write.write_all(format!("JOIN {c}\r\n", c = CHANNEL).as_bytes()).unwrap();
+	irc.write.flush();
 }
